e404dc7c0c89e3394a71c0008a6f0730326a23cc
Julian <mrjulik.beletsky@yandex.ru>
2024-04-16 00:22:38 +0300

    BuildingsSliders

---
+++ Assets/Scripts/Spawner/MergerSliderControllercs.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DefaultExecutionOrder(5)]
+public class MergerSliderControllercs : MonoBehaviour
+{
+    [SerializeField] private Slider _resourcesSlider;
+    [SerializeField] private TMP_Text _resourcesText;
+
+    [SerializeField] private Merger _merger;
+
+    private int _maxResources;
+
+    private void Start()
+    {
+        _maxResources = _merger.CurrentResourceCount.Value;
+
+        _resourcesSlider.maxValue = _maxResources;
+        _resourcesSlider.value = _resourcesSlider.maxValue;
+        _resourcesText.text = _maxResources + "/" + _maxResources;
+
+        _merger.CurrentResourceCount.AddListener(OnResourcesChanged);
+    }
+
+    private void OnDestroy()
+    {
+        _merger.CurrentResourceCount.RemoveListener(OnResourcesChanged);
+    }
+
+    private void OnResourcesChanged(int resource)
+    {
+        _resourcesSlider.value = resource;
+        _resourcesText.text = resource + "/" + _maxResources;
+    }
+}
---
+++ Assets/Scripts/Spawner/SpawnerSliderController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DefaultExecutionOrder(5)]
+public class SpawnerSliderController : MonoBehaviour
+{
+    [SerializeField] private Slider _hpSlider;
+    [SerializeField] private TMP_Text _hpText;
+    [SerializeField] private Slider _resourcesSlider;
+    [SerializeField] private TMP_Text _resourcesText;
+    [SerializeField] private Slider _timerSlider;
+    [SerializeField] private TMP_Text _timerText;
+    
+    [SerializeField] private Spawner _spawner;
+
+    private int _maxHp;
+    private int _maxResources;
+    private float _maxTimeSpawn;
+
+    private void Start()
+    {
+        _maxHp = _spawner.HealthComp.Health.Value;
+        _maxResources = _spawner.CurrentResourceCount.Value;
+        _maxTimeSpawn = _spawner.CurrentTimeBeforeSpawn.Value;
+
+        _hpSlider.maxValue = _maxHp;
+        _hpSlider.value = _hpSlider.maxValue;
+        _hpText.text = _maxHp + "/" + _maxHp;
+        _resourcesSlider.maxValue = _maxResources;
+        _resourcesSlider.value = _resourcesSlider.maxValue;
+        _resourcesText.text = _maxResources + "/" + _maxResources;
+        _timerSlider.maxValue = _maxTimeSpawn;
+        _timerSlider.value = 0;
+        _timerText.text = _spawner.CurrentTimeBeforeSpawn + " sec";
+
+        _spawner.HealthComp.Health.AddListener(OnHpChanged);
+        _spawner.CurrentTimeBeforeSpawn.AddListener(OnTimerChanged);
+        _spawner.CurrentResourceCount.AddListener(OnResourcesChanged);
+    }
+
+    private void OnDestroy()
+    {
+        _spawner.HealthComp.Health.RemoveListener(OnHpChanged);
+        _spawner.CurrentTimeBeforeSpawn.RemoveListener(OnTimerChanged);
+        _spawner.CurrentResourceCount.RemoveListener(OnResourcesChanged);
+    }
+
+    private void OnHpChanged(int hp)
+    {
+        _hpSlider.value = hp;
+        _hpText.text = hp + "/" + _maxHp;
+    }
+
+    private void OnResourcesChanged(int resource)
+    {
+        _resourcesSlider.value = resource;
+        _resourcesText.text = resource + "/" + _maxResources;
+    }
+
+    private void OnTimerChanged(float time)
+    {
+        _timerSlider.value = _maxTimeSpawn - time;
+        _timerText.text = String.Format("{0:0.00}", time) + " sec";
+    }
+}
